32c3db12b3085f68dd0259986a3156bab3e89572
rafael.schlemper <rafael.schlemper@gennera.com.br>
2017-08-01 09:08:34 -0300

    ajustes sql

--- sql/init.sql
+++ sql/init.sql
@@ -2,7 +2,33 @@ CREATE TABLE usuario (
 	id INT NOT NULL AUTO_INCREMENT,
 	nome VARCHAR(200) NOT NULL,
 	idade INTEGER NOT NULL,
-	telefone INTEGER NOT NULL,
 	data DATETIME NOT NULL,	
    	PRIMARY KEY ( id )
 );
+
+CREATE TABLE telefone (
+	id INT NOT NULL AUTO_INCREMENT,
+	codigoArea INT NOT NULL,
+	numero INT NOT NULL,
+   	PRIMARY KEY ( id )
+);
+
+CREATE TABLE usuario_telefone (
+	idUsuario INT NOT NULL,
+	idTelefone INT NOT NULL,
+    primary key(idUsuario, idTelefone)
+);
+
+ALTER TABLE `usuario_telefone` 
+ADD CONSTRAINT `fk_usuario_telefone`
+  FOREIGN KEY (`idUsuario`)
+  REFERENCES `usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `usuario_telefone` 
+ADD CONSTRAINT `fk_telefone_usuario`
+  FOREIGN KEY (`idTelefone`)
+  REFERENCES `telefone` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
